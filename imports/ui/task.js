bcb928499180e2c92cf6aefe577d69aa66eb5576
Mathias Artur Schulz <mathias@conectra.net.br>
2021-05-24 08:48:55 -0300

    [UPD] Atualizado a segurança dos métodos

--- imports/ui/task.js
+++ imports/ui/task.js
@@ -1,5 +1,5 @@
 import { Template } from 'meteor/templating';
-
+import { Meteor } from 'meteor/meteor';
 import { Tasks } from '../api/tasks.js';
 
 import './task.html';
@@ -7,11 +7,9 @@ import './task.html';
 Template.task.events({
   'click .toggle-checked'() {
     // Set the checked property to the opposite of its current value
-    Tasks.update(this._id, {
-      $set: { checked: ! this.checked },
-    });
+    Meteor.call('tasks.setChecked', this._id, !this.checked);
   },
   'click .delete'() {
-    Tasks.remove(this._id);
+    Meteor.call('tasks.remove', this._id);
   },
 });
